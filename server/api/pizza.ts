2d6fe4cf47e356ff0da0de54799620aebfd93b97
stephan-roomle <79909204+stephan-roomle@users.noreply.github.com>
2023-03-03 10:02:08 +0100

    wip: worked on pizza service

--- server/api/pizza.ts
+++ server/api/pizza.ts
@@ -1,39 +1,28 @@
-/*import {MongoClient} from 'mongodb';
-
-const username = 'stephanschober';
+import { MongoClient } from 'mongodb';
 
 // Connection URI to your MongoDB Atlas cluster
-const uri = `mongodb+srv://stephanschober:${process.env.MONGO_DB_PASSSWORD}@https://cloud.mongodb.com/v2/633e71501dba1531754b6a49#/metrics/host/161a4eca99ccd88aa070cfdcbbe52611/status.mongodb.net/<database>?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://stephanschober:${process.env.MONGO_DB_PASSSWORD}@carbone.ssmc7li.mongodb.net/carbone?retryWrites=true&w=majority`;
 
 // MongoDB Atlas database and collection names
-const dbName = '<database>';
+const dbName = 'carbone';
 const collectionName = 'carbone_orders';
 
 // Create a new MongoClient
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+  const { name } = getQuery(event);
+
   const db = client.db(dbName);
   const collection = db.collection(collectionName);
 
-  // Find a document in the collection
-  collection.findOne({ orderNumber: '12345' }, (err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    // Log the result to the console
-    console.log(result);
-
-    // Close the client connection
+  let result;
+  try {
+    // Find a document in the collection
+    result = await collection.findOne({ username: name });
+  } finally {
     client.close();
-  });
+  }
 
-
-  const { name } = getQuery(event);
-  return {
-    name,
-  };
+  return result;
 });
-*/
